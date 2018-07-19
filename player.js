f1e3a954652ef133aff07e160ab2008fe8f3212d
diathelia <37617288+diathelia@users.noreply.github.com>
2018-07-20 02:54:41 +1200

    Update player.js

--- player.js
+++ player.js
@@ -15,7 +15,7 @@ function onYouTubeIframeAPIReady() {
   player = new YT.Player("player", {
     height: "100%",
     width: "100%",
-    playerVars: { origin: "127.0.0.1:5500" }
+//     playerVars: { origin: "127.0.0.1:5500" }
     // videoId: "",
     // events: {
     // onReady: onPlayerReady
